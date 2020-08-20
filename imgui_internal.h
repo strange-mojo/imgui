3c8af169a59a64f7cb82395ee011fe23e6ed7af5
ocornut <omarcornut@gmail.com>
2020-08-20 20:46:57 +0200

    RangeSelect/MultiSelect: Temporary fix/work-around for child/popup to not inherit MultiSelectEnabled flag, until we make mulit-select data stackable.

--- imgui_internal.h
+++ imgui_internal.h
@@ -1392,9 +1392,9 @@ struct ImGuiContext
     bool                    FocusTabPressed;                    //
 
     // Range-Select/Multi-Select
-    bool                    MultiSelectEnabled;
+    ImGuiWindow*            MultiSelectEnabledWindow;           // FIXME-MULTISELECT: We currently don't support recursing/stacking multi-select
     ImGuiMultiSelectFlags   MultiSelectFlags;
-    ImGuiMultiSelectState   MultiSelectState;                   // We currently don't support recursing/stacking multi-select
+    ImGuiMultiSelectState   MultiSelectState;                   
     ImGuiKeyModFlags        MultiSelectKeyMods;
 
     // Render
@@ -1584,7 +1584,7 @@ struct ImGuiContext
         FocusRequestNextCounterRegular = FocusRequestNextCounterTabStop = INT_MAX;
         FocusTabPressed = false;
 
-        MultiSelectEnabled = false;
+        MultiSelectEnabledWindow = NULL;
         MultiSelectFlags = ImGuiMultiSelectFlags_None;
         MultiSelectKeyMods = ImGuiKeyModFlags_None;
 
