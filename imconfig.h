22b4201d189b767afe17104170e7c435f4f56e81
Nick Phillips <nick@strangemojo.com>
2020-12-20 00:24:49 -0500

    config and addons

--- imconfig.h
+++ imconfig.h
@@ -14,6 +14,14 @@
 
 #pragma once
 
+#define IMGUI_OVERRIDE_DRAWVERT_STRUCT_LAYOUT struct ImDrawVert\
+{\
+    ImVec2  pos;\
+    float   posZ;\
+    ImU32   col;\
+    ImVec2  uv;\
+};
+
 //---- Define assertion handler. Defaults to calling assert().
 // If your macro uses multiple statements, make sure is enclosed in a 'do { .. } while (0)' block so it can be used as a single statement.
 //#define IM_ASSERT(_EXPR)  MyAssert(_EXPR)
